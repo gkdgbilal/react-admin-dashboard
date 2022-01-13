739ef05c8d796aa51b3d80c06b63710f886e6cd3
bilal <bilal.gkdg@gmail.com>
2022-01-13 16:11:07 +0300

    Latest changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar.jsx';
-import './app.css';
+import './styles/app.css';
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserList from './pages/userList/UserList';
